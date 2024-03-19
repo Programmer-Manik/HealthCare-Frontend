47e4430204cd3a9738e109611fadaebe0227aa67
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-03-19 16:11:48 +0600

    Fetch Specialties Data And Create Specialties Section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/ui/HomePage/HeroSection/HeroSection";
+import Specialist from "@/components/ui/HomePage/Specialist/Specialist";
 import { Button } from "@mui/material";
 
 const HomePage = () => {
   return (
     <div>
      <HeroSection/>
+     <Specialist/>
     </div>
   );
 };
---
+++ src/components/ui/HomePage/Specialist/Specialist.tsx
@@ -0,0 +1,35 @@
+import { Box, Container, Typography } from "@mui/material";
+import { start } from "repl";
+
+const Specialist = async() => {
+ const res = await fetch('http://localhost:5000/api/v1/specialties',{
+   next:{
+      revalidate:30,
+   }
+ });
+ const specialties = await res.json()
+//  console.log(specialties)
+   return (
+    <Container>
+      <Box
+        sx={{
+          margin: "40px 0px",
+          textAlign: "center",
+        }}
+      >
+        <Box sx={{
+         textAlign:"start",
+        }}>
+          <Typography variant="h4" fontWeight={600}>
+            Explore Treatments Across Specialties
+          </Typography>
+          <Typography component="p" fontWeight={300} fontSize={18}>
+            Experienced Doctors Across All Specialties
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Specialist;
