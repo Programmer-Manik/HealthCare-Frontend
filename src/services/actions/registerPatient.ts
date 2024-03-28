8689e7d3ae3849c47284402bd148beee28df29da
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-03-29 03:18:54 +0600

    user create patient and server add data

--- src/services/actions/registerPatient.ts
+++ src/services/actions/registerPatient.ts
@@ -1,5 +1,10 @@
 "use server"
 const RegisterPatient = async(formData:FormData) => {
+   const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/user/create-patient`,{
+      method:'POST',
+      body:formData,
+      cache:'no-store'
+   })
   return 
 };
 
