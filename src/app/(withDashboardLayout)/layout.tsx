9cf4fa1cbca8582e63785a7eb24fe45bc141c2b2
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-04-12 22:45:15 +0600

    with dashboard layout

--- src/app/(withDashboardLayout)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardDrawer from "@/components/Dashboard/DashboardDrawer/DashboardDrawer";
-import React from "react";
-
-const DashboardLayout = ({children}:{children:React.ReactNode}) => {
-  return <DashboardDrawer>{children}</DashboardDrawer>
-  
-};
-
-export default DashboardLayout;
