805227ffa3fad34e781c6c94aaadf2d6f1d57da9
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-04-14 21:52:36 +0600

    Cannot read properties of undefined

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,22 +1,35 @@
-"use client"
+"use client";
 import { Box, Button, Stack, TextField } from "@mui/material";
 import { useState } from "react";
 import SpecialtyModal from "./components/SpecialistModal";
 import { useGetAllSpecialtyQuery } from "@/redux/api/specialtiesApi";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 const SpecialtiesPage = () => {
   const [isModalOpen, setISModalOpen] = useState(false);
-  const {data , isLoading }= useGetAllSpecialtyQuery({});
+  const { data, isLoading } = useGetAllSpecialtyQuery({});
   // console.log(data)
+  const columns: GridColDef[] = [{ field: "title", headerName: "Title", width: 70 }];
+
   return (
     <Box>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
         <Button onClick={() => setISModalOpen(true)}>Create Specialty</Button>
-        <SpecialtyModal open={isModalOpen} setOpen={setISModalOpen}/>
+        <SpecialtyModal open={isModalOpen} setOpen={setISModalOpen} />
         <TextField size="small" placeholder="Search Specialties" />
       </Stack>
       <Box>
-        <h1>Display Specialty</h1>
+        <DataGrid
+          rows={data}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+        />
       </Box>
     </Box>
   );
