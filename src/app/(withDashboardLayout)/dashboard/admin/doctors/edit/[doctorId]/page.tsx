d1130e8d8b765c5c6766c65d6667862883a74010
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-04-26 14:13:38 +0600

    Updating Doctor Information

--- src/app/(withDashboardLayout)/dashboard/admin/doctors/edit/[doctorId]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/doctors/edit/[doctorId]/page.tsx
@@ -20,18 +20,18 @@ type TParams = {
 
 const DoctorUpdatePage = ({ params }: TParams) => {
   const router = useRouter();
-   const id = params?.doctorId
-  const { data, isLoading } = useGetDoctorQuery(params?.doctorId);
+  const id = params?.doctorId;
+  const { data, isLoading } = useGetDoctorQuery(id);
   const [updateDoctor] = useUpdateDoctorMutation();
   // console.log(data)
   const handleFormSubmit = async (values: FieldValues) => {
-     values.experience = Number(values.experience);
-     values.apointmentFee = Number(values.apointmentFee);
-     values.id = id;
+    values.experience = Number(values.experience);
+    values.apointmentFee = Number(values.apointmentFee);
+    values.id = id;
     // console.log({ id: values.id, body: values });
 
     try {
-       const res = await updateDoctor({ id: values.id, body: values }).unwrap();
+      const res = await updateDoctor({ id: values.id, body: values }).unwrap();
       if (res?.id) {
         toast.success("Doctor Updated Successfully!!!");
         router.push("/dashboard/admin/doctors");
@@ -58,102 +58,114 @@ const DoctorUpdatePage = ({ params }: TParams) => {
   return (
     <Box>
       <Typography component="h5" variant="h5">
-        doctor Update Page
+        doctor Update info
       </Typography>
-      <MHForm onSubmit={handleFormSubmit} defaultValues={defaultValues}>
-        <Grid container spacing={2} sx={{ my: 5 }}>
-          <Grid item xs={12} sm={12} md={4}>
-            <MHInput name="name" label="Name" fullWidth={true} sx={{ mb: 2 }} />
-          </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            <MHInput
-              name="email"
-              type="email"
-              label="Email"
-              fullWidth={true}
-              sx={{ mb: 2 }}
-            />
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <MHForm
+          onSubmit={handleFormSubmit}
+          defaultValues={data && defaultValues}
+        >
+          <Grid container spacing={2} sx={{ my: 5 }}>
+            <Grid item xs={12} sm={12} md={4}>
+              <MHInput
+                name="name"
+                label="Name"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <MHInput
+                name="email"
+                type="email"
+                label="Email"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={4}>
+              <MHInput
+                name="contactNumber"
+                label="Contract Number"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <MHInput
+                name="address"
+                label="Address"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <MHInput
+                name="registrationNumber"
+                label="Registration Number"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <MHInput
+                name="experience"
+                type="number"
+                label="Experience"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <MHSelectField
+                items={Gender}
+                name="gender"
+                label="Gender"
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <MHInput
+                name="apointmentFee"
+                type="number"
+                label="ApointmentFee"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <MHInput
+                name="qualification"
+                label="Qualification"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={4}>
+              <MHInput
+                name="currentWorkingPlace"
+                label="Current Working Place"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <MHInput
+                name="designation"
+                label="Designation"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
           </Grid>
 
-          <Grid item xs={12} sm={12} md={4}>
-            <MHInput
-              name="contactNumber"
-              label="Contract Number"
-              fullWidth={true}
-              sx={{ mb: 2 }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            <MHInput
-              name="address"
-              label="Address"
-              fullWidth={true}
-              sx={{ mb: 2 }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            <MHInput
-              name="registrationNumber"
-              label="Registration Number"
-              fullWidth={true}
-              sx={{ mb: 2 }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            <MHInput
-              name="experience"
-              type="number"
-              label="Experience"
-              fullWidth={true}
-              sx={{ mb: 2 }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            <MHSelectField
-              items={Gender}
-              name="gender"
-              label="Gender"
-              sx={{ mb: 2 }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            <MHInput
-              name="apointmentFee"
-              type="number"
-              label="ApointmentFee"
-              fullWidth={true}
-              sx={{ mb: 2 }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            <MHInput
-              name="qualification"
-              label="Qualification"
-              fullWidth={true}
-              sx={{ mb: 2 }}
-            />
-          </Grid>
-
-          <Grid item xs={12} sm={12} md={4}>
-            <MHInput
-              name="currentWorkingPlace"
-              label="Current Working Place"
-              fullWidth={true}
-              sx={{ mb: 2 }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            <MHInput
-              name="designation"
-              label="Designation"
-              fullWidth={true}
-              sx={{ mb: 2 }}
-            />
-          </Grid>
-        </Grid>
-
-        <Button type="submit">Update</Button>
-      </MHForm>
+          <Button type="submit">Update</Button>
+        </MHForm>
+      )}
     </Box>
   );
 };
