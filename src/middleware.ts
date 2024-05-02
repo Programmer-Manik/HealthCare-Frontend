1ff43d6dfb5e1858d30516cf48f5bf3636322ce3
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-05-02 09:25:03 +0600

    Route Protection In NextJs With Nextjs Middleware Part-3

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
-
+import { jwtDecode } from 'jwt-decode';
 type Role = keyof typeof roleBasedPrivateRoutes;
 
 const AuthRoutes = ['/login', '/register'];
@@ -33,7 +33,24 @@ export function middleware(request: NextRequest) {
       return NextResponse.next();
    }
 
+   let decodedData = null;
 
+   if (accessToken) {
+      decodedData = jwtDecode(accessToken) as any;
+   }
+
+   const role = decodedData?.role;
+
+   // if (role === 'ADMIN' && pathname.startsWith('/dashboard/admin')) {
+   //    return NextResponse.next();
+   // }
+
+   if (role && roleBasedPrivateRoutes[role as Role]) {
+      const routes = roleBasedPrivateRoutes[role as Role];
+      if (routes.some((route) => pathname.match(route))) {
+         return NextResponse.next();
+      }
+   }
 
 
   return NextResponse.redirect(new URL('/', request.url))
