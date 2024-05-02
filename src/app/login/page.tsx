962f7cf98aee6f2801e015542393a99021c36d99
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-05-02 20:03:30 +0600

    Implementing Forgot Password Part-1

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,11 +11,15 @@ import {
 import Image from "next/image";
 import assets from "@/assets";
 import Link from "next/link";
-import { FieldValue, FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import {
+  FieldValue,
+  FieldValues,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
 import { userLogin } from "@/services/actions/userLogin";
 import { storeUserInfo } from "@/services/auth.services";
 import { toast } from "sonner";
-import { Router } from "next/router";
 import { useRouter } from "next/navigation";
 import MHForm from "@/components/Forms/MHForm";
 import MHInput from "@/components/Forms/MHInput";
@@ -23,17 +27,17 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 
-export const ValidationSchema  = z.object({
-  email: z.string().email('Please enter valid a email address!'),
-  password:z.string().min(6,'Must be at least 6 characters')
-})
+export const ValidationSchema = z.object({
+  email: z.string().email("Please enter valid a email address!"),
+  password: z.string().min(6, "Must be at least 6 characters"),
+});
 
 const LoginPage = () => {
   const router = useRouter();
 
-  const [error, setError] = useState("")
- 
-  const handleLogin = async (values:FieldValues) => {
+  const [error, setError] = useState("");
+
+  const handleLogin = async (values: FieldValues) => {
     // console.log(values);
     try {
       const res = await userLogin(values);
@@ -41,8 +45,8 @@ const LoginPage = () => {
         toast.success(res?.message);
         storeUserInfo({ accessToken: res?.data?.accessToken });
         // router.push("/dashboard");
-      }else{
-        setError(res.message)
+      } else {
+        setError(res.message);
       }
     } catch (err: any) {
       console.error(err.message);
@@ -85,28 +89,32 @@ const LoginPage = () => {
           </Stack>
           {error && (
             <Box>
-            <Typography sx={{
-              backgroundColor:'red',
-              padding:'1px',
-              borderRadius:'2px',
-              color:'white',
-              marginTop:'5px'
-            }}>{error}</Typography>
-          </Box>
+              <Typography
+                sx={{
+                  backgroundColor: "red",
+                  padding: "1px",
+                  borderRadius: "2px",
+                  color: "white",
+                  marginTop: "5px",
+                }}
+              >
+                {error}
+              </Typography>
+            </Box>
           )}
           <Box>
-            <MHForm 
-            onSubmit={handleLogin}
-            resolver={zodResolver(ValidationSchema)}
-            defaultValues={{
-              email:'',
-              password:'',
-            }}
+            <MHForm
+              onSubmit={handleLogin}
+              resolver={zodResolver(ValidationSchema)}
+              defaultValues={{
+                email: "",
+                password: "",
+              }}
             >
               <Grid container spacing={2} my={1}>
                 <Grid item md={6}>
                   <MHInput
-                  name="email"
+                    name="email"
                     label="Email"
                     type="email"
                     fullWidth={true}
@@ -114,18 +122,27 @@ const LoginPage = () => {
                 </Grid>
                 <Grid item md={6}>
                   <MHInput
-                   name="password"
+                    name="password"
                     label="Password"
                     type="password"
                     fullWidth={true}
-                 
                   />
                 </Grid>
               </Grid>
 
-              <Typography mb={1} textAlign="end" component="p" fontWeight={300}>
-                Forgot Password?
-              </Typography>
+              <Link href={"/forgot-password"}>
+                <Typography
+                  mb={1}
+                  textAlign="end"
+                  component="p"
+                  fontWeight={300}
+                  sx={{
+                    textDecoration:"underline",
+                  }}
+                >
+                  Forgot Password?
+                </Typography>
+              </Link>
 
               <Button
                 sx={{
